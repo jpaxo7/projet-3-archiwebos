fa2af1fb770272ef5c00800cf791727484f804d9
scaro7 <jeanpierresabah@gmail.com>
2024-06-20 19:01:50 +0200

    modification des fonctions d'ouverture et fermeture des modales

--- Frontend/scripts/modal.js
+++ Frontend/scripts/modal.js
@@ -4,27 +4,23 @@ let works=[];
 //fonction d'ouverture de la modale
 const openModal = function (event) {
     event.preventDefault();
-    modal = document.querySelector(event.target.getAttribute('href'));
+    modal = document.querySelector(".modal");
     modal.style.display = "flex";
-    modal.addEventListener('click', closeModal);
-    modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
-    modal.querySelector('.modal-wrapper').addEventListener('click', stopPropagation);
+    debugger;
+    if ((event.target.matches(".js-close-modal") || !event.target.closest(".modal")) && !event.target.matches(".js-modal")){
+        closeModal();
+    };
+    // modal.querySelector('.js-close-modal').addEventListener('click', closeModal);
 };
 
 //fonction de fermeture de la modale
 const closeModal = function (event){
     if (modal === null) return;
-    event.preventDefault();
+    // event.preventDefault();
     modal.style.display = "none";
     modal.removeEventListener('click', closeModal);
     modal.querySelector('.js-close-modal').removeEventListener('click', closeModal);
-    modal.querySelector('.modal-wrapper').removeEventListener('click', stopPropagation);
     modal = null;
-};
-
-//fonction permettant de fermer la modale en cas de click en dehors de la boite modale
-const stopPropagation = function (event){
-    event.stopPropagation();
 };
 
 //ouverture de la modale à l'écoute du click
